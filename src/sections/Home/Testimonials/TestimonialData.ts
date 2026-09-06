5bda40a39745f063139ada76d1738d5f96f96e44
swathi <swathihsnk26@gmail.com>
2026-09-06 20:40:44 +0530

    Reload Issue

--- src/sections/Home/Testimonials/TestimonialData.ts
+++ src/sections/Home/Testimonials/TestimonialData.ts
@@ -12,7 +12,7 @@ export const testimonials: Testimonial[] = [
   {
     id: 1,
     name: "Div Mittal",
-    role: "Founder, HumTum Media Inc.",
+    role: "Founder, HumTum Media Inc, Canada",
     review:
       "Gopal has been incredible to work with: super reliable, with smooth and seamless communication from start to finish. The quality of his 3D work was top-notch, perfectly capturing our vision. Highly recommended!",
     image: "/images/testimonials/Div.jpg",
@@ -44,8 +44,8 @@ export const testimonials: Testimonial[] = [
   },
   {
     id: 5,
-    name: "Maari Prabhakaran",
-    role: "MD - Maari Constructions",
+    name: "Mari Prabhakaran",
+    role: "Mari Constructions, Coimbatore",
     review:
       "Mr. Gopalakrishnan consistently delivers elevation on time. We discuss several changes together, and he implements them patiently and thoroughly, which helps me achieve a 100% satisfactory output. I’m very happy with his work, and we’re looking forward to making our upcoming projects even better.",
     image: "/images/testimonials/Mari.jpg",
